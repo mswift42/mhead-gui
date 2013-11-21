e2cda11d04e6ba128ec9fd65e52beac00b3369f1
mswift42 <martin.haesler@gmail.com>
2013-11-21 16:37:00 +0000

    Changed to hunchentoot and cl-who.

--- mheadgui.lisp
+++ mheadgui.lisp
@@ -1,7 +1,43 @@
-(ql:quickload '(:restas :html-template))
+(ql:quickload '(:hunchentoot :cl-who :parenscript))
 
 (defpackage #:mheadgui
-  (:use :cl :restas :html-template))
+  (:use :cl  :cl-who :parenscript :hunchentoot))
 
 (in-package #:mheadgui)
+
+(defmacro with-html (&body body)
+    `(with-html-output (*standard-output* nil)
+       ,@body))
+
+(defmacro with-html-string (&body body)
+  `(with-html-output-to-string (*standard-output* nil)
+     ,@body))
+
+
+(push (hunchentoot:create-static-file-dispatcher-and-handler
+       "/heavy.css" "heavy.css") hunchentoot:*dispatch-table*)
+
+(defun main-page ()
+  (with-html
+    (:head
+     (:title "MetalHead!")
+     (:link :type "text/css" :rel "stylesheet"
+	    :href "/heavy.css"))
+    (:body
+     (:h3 :class "header" "MetalHead!")
+     (:div :class "textarea"
+	   (:form :method :post
+		  (:textarea :rows "30" :cols "40" :name "tarea" :class "tarea"))))))
+
+(define-easy-handler (mainpage :uri "/MetalHead" )
+    ()
+  (main-page))
+
+(defvar *web-server* (make-instance 'easy-acceptor :port 4343))
+
+
+
+
+
+
 
