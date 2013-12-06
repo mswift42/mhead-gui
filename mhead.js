2c70bdfcee2f95af7449904c3d35cdcac98b1a07
mswift42 <martin.haesler@gmail.com>
2013-12-06 14:32:15 +0000

    Added appendText function.

--- mhead.js
+++ mhead.js
@@ -1,6 +1,13 @@
-function appendText(dest, s) {
+function setText(dest, s) {
     // append a string s to a given destionation dest
     document.getElementById(dest).value=s;
     };
+
+function appendText(s) {
+    // append text s to textarea
+    document.getElementById("tarea").value = document.getElementById("tarea").value + s;
+};
+
+
 
     
