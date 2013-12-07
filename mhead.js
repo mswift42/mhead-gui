6fe8744edc77ccf66f4c63d5e318968e328a2bb5
mswift42 <martin.haesler@gmail.com>
2013-12-07 10:17:14 +0000

    Added getInpText function.

--- mhead.js
+++ mhead.js
@@ -1,13 +1,19 @@
 function setText(dest, s) {
     // append a string s to a given destionation dest
     document.getElementById(dest).value=s;
-    };
+};
 
 function appendText(s) {
     // append text s to textarea
     document.getElementById("tarea").value = document.getElementById("tarea").value + s;
 };
 
+function getInputText() {
+    // return submitted text of submit field.
+    return document.getElementById("inptext").value;
+};
 
 
-    
+
+
+
